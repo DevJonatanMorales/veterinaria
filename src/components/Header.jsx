a8f84c4b75e258059606f1841f30137f186d761c
Jonatan Morales <developerjonatanmorales@gmail.com>
2023-11-05 00:47:07 -0600

    Add new files and update configurations

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+const [state, setStaet] = useState({});
+export const Header = () => {
+  return <div>Header</div>;
+};
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+const [state, setStaet] = useState({});
+export const Listado = () => {
+  return (
+    <div>
+      Listado
+    </div>
+  );
+}
