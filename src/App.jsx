6a4a4527b6de5cadaca1ec50c39c261a716cf9bc
Jonatan Morales <developerjonatanmorales@gmail.com>
2023-11-15 02:49:15 -0600

    Add Tailwind CSS and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Header } from './components/Header';
 import { Listado } from './components/ListadoPacientes';
 
 function App() {
-	return <>
+	return <div className='container mx-auto mt-20' >
     <Header />
     <Formulario />
     <Listado />
-  </>;
+  </div>;
 }
 
 export default App;
