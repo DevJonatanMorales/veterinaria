daa981121715a6dbb1715d6f72bdc83a6e14b104
Jonatan Morales <developerjonatanmorales@gmail.com>
2023-11-22 02:43:48 -0600

    Refactor ListadoPacientes component to remove unnecessary overflow-y-scroll class

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,7 +3,7 @@ import { Pacientes } from './Pacientes';
 
 export const Listado = ({ pacientes, setPaciente, eliminaPaciente }) => {
 	return (
-		<div className='md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll'>
+		<div className='md:w-1/2 lg:3/5'>
 			{pacientes && pacientes.length ? (
 				<>
 					<h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
@@ -13,14 +13,16 @@ export const Listado = ({ pacientes, setPaciente, eliminaPaciente }) => {
 							Pacientes y Citas
 						</span>
 					</p>
-					{pacientes.map(paciente => (
-						<Pacientes
-							key={paciente.id}
-							paciente={paciente}
-							setPaciente={setPaciente}
-							eliminaPaciente={eliminaPaciente}
-						/>
-					))}
+					<div className='md:h-screen overflow-y-scroll'>
+						{pacientes.map(paciente => (
+							<Pacientes
+								key={paciente.id}
+								paciente={paciente}
+								setPaciente={setPaciente}
+								eliminaPaciente={eliminaPaciente}
+							/>
+						))}
+					</div>
 				</>
 			) : (
 				<>
