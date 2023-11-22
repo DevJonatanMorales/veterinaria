fbd5f9badab0941cc1ae22adf4c1bac510054584
Jonatan Morales <developerjonatanmorales@gmail.com>
2023-11-22 01:16:45 -0600

    Update error component styling

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Error = ({children}) => {
 	return (
-		<div className='bg-red-800 text-white text-center font-bold p-3 mb-3 rounded-md'>
+		<div className='bg-red-500 text-white text-center font-bold p-3 mb-3 rounded-md'>
 			<p>{children}</p>
 		</div>
 	);
